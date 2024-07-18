166e42df9103e3c39c45adca47159105c13a0230
DESKTOP-8PTGRH7\User <bw812810@gmail.com>
2024-07-18 17:44:34 +0800

    第一階段-課程

---
+++ client/src/components/course-component.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import CourseService from "../services/course-service";
+
+//有登入當前使用者
+const CourseComponent = ({ currentUser }) => {
+  const navigate = useNavigate();
+  const hanldeTakeToLogin = () => {
+    navigate("/login");
+  };
+
+  //搜尋講師id
+  const [courseData, setCourseData] = useState(null);
+  useEffect(() => {
+    let _id;
+    if (currentUser) {
+      _id = currentUser.user._id;
+      if (currentUser.user.role == "instructor") {
+        CourseService.get(_id)
+          .then((data) => {
+            setCourseData(data.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else if (currentUser.user.role == "student") {
+        CourseService.getEnrolledCourse(_id)
+          .then((data) => {
+            console.log(data);
+            setCourseData(data.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+    }
+  }, []);
+
+  return (
+    <div style={{ padding: "3rem" }}>
+      {!currentUser && (
+        <div>
+          <p>您必須先登入才能看到課程</p>
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={hanldeTakeToLogin}
+          >
+            回到登入頁面
+          </button>
+        </div>
+      )}
+
+      {/* 有當前用戶 且 身分是學生 */}
+      {currentUser && currentUser.user.role == "instructor" && (
+        <div>
+          <h1>歡迎來到講師課程頁面</h1>
+        </div>
+      )}
+      {/* 有當前用戶 且 身分是講師 */}
+      {currentUser && currentUser.user.role == "student" && (
+        <div>
+          <h1>歡迎來到學生課程頁面</h1>
+        </div>
+      )}
+
+      {/* 有當前用戶 且 有註冊課程 且 至少有註冊一個課程  */}
+      {currentUser && courseData && courseData.length != 0 && (
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {courseData.map((course) => {
+            return (
+              <div className="card" style={{ width: "18rem ", margin: "1rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">課程名稱:{course.title}</h5>
+                  <p style={{ margin: "0.5rem 0rem" }} className="card-text">
+                    {course.description}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    學生人數:{course.students.length}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    課程價格:{course.price}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CourseComponent;
---
+++ client/src/services/course-service.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+const API_URL = "http://localhost:8080/api/courses";
+
+class CourseService {
+  //已登入為講師 在本到找token 要新增課程
+  post(title, description, price) {
+    let token;
+    if (localStorage.getItem("user")) {
+      token = JSON.parse(localStorage.getItem("user")).token;
+    } else {
+      token = "";
+    }
+    return axios.post(
+      API_URL,
+      {
+        title,
+        description,
+        price,
+      },
+      { headers: { Authorization: token } }
+    );
+  }
+
+  //找到學生註冊的課程
+  getEnrolledCourse(_id) {
+    let token;
+    if (localStorage.getItem("user")) {
+      token = JSON.parse(localStorage.getItem("user")).token;
+    } else {
+      token = "";
+    }
+
+    return axios.get(API_URL + "/student/" + _id, {
+      headers: { Authorization: token },
+    });
+  }
+
+  //請求是尋找講師id時;
+  get(_id) {
+    let token;
+    if (localStorage.getItem("user")) {
+      token = JSON.parse(localStorage.getItem("user")).token;
+    } else {
+      token = "";
+    }
+    return axios.get(API_URL + "/instructor/" + _id, {
+      headers: { Authorization: token },
+    });
+  }
+  //請求是尋找課程名稱
+  getCourseByName(name) {
+    let token;
+    if (localStorage.getItem("user")) {
+      token = JSON.parse(localStorage.getItem("user")).token;
+    } else {
+      token = "";
+    }
+    return axios.get(API_URL + "/findByName/" + name, {
+      headers: { Authorization: token },
+    });
+  }
+
+  //請求註冊課程id
+  enrollCourses(_id) {
+    let token;
+    if (localStorage.getItem("user")) {
+      token = JSON.parse(localStorage.getItem("user")).token;
+    } else {
+      token = "";
+    }
+    //怎麼要一個空物件??
+    return axios.post(
+      API_URL + "/enroll/" + _id,
+      {},
+      {
+        headers: { Authorization: token },
+      }
+    );
+  }
+}
+
+export default new CourseService();
